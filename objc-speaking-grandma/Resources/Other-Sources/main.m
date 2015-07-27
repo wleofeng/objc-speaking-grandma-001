5472ded2990438d49a07d95a4d042453deed05d6
markedwardmurray <markedwardmurray@gmail.com>
2015-07-27 15:20:23 -0400

    Initial Commit

---
+++ objc-speaking-grandma/Resources/Other-Sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objc-speaking-grandma
+//
+//  Created by iOS Staff on 7/27/15
+//  Copyright (c) 2015 The Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
