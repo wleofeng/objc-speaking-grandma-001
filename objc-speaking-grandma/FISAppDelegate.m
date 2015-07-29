515bd788a9e9e9fe45e4f2a40e1148707ab5bc0c
markedwardmurray <markedwardmurray@gmail.com>
2015-07-28 22:38:19 -0400

    remove solution

--- objc-speaking-grandma/FISAppDelegate.m
+++ objc-speaking-grandma/FISAppDelegate.m
@@ -24,46 +24,6 @@
      
      */
     
-    // Code-Along I: Talk To Grandma
-    
-    NSString *talkToGrandma = @"HI, GRANDMA!";
-    NSString *shoutAtGrandma = [talkToGrandma uppercaseString];
-    BOOL shouting = [talkToGrandma isEqualToString:shoutAtGrandma];
-    
-    /** Advanced
-    talkToGrandma = @"Hi, Grandma!";
-    shoutAtGrandma = [talkToGrandma uppercaseString];
-     */
-    
-    if (shouting) {
-        NSLog(@"NO, NOT SINCE 1938!");
-    } else {
-        NSLog(@"WHAT'S THAT? SPEAK UP, SONNY!");
-    }
-
-    // Code-Along II: Direct Evaluation
-    
-    if ([talkToGrandma isEqualToString:shoutAtGrandma]) {
-        NSLog(@"NO, NOT SINCE 1938!");
-    } else {
-        NSLog(@"WHAT'S THAT? SPEAK UP, SONNY!");
-    }
-    
-    // Code-Along III: Negative Checks
-
-    if (!shouting) {
-        NSLog(@"WHAT'S THAT? SPEAK UP, SONNY!");
-    } else {
-        NSLog(@"NO, NOT SINCE 1938!");
-    }
-    
-    if (![talkToGrandma isEqualToString:shoutAtGrandma]) {
-        NSLog(@"WHAT'S THAT? SPEAK UP, SONNY!");
-    } else {
-        NSLog(@"NO, NOT SINCE 1938!");
-    }
-
-    
     return YES; // Don't alter this statement.
 }
 
